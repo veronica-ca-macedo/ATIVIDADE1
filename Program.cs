7a02cd645b96de0e1ded246a914306b78a296d44
Verônica Caldeira Azambuja Macedo <veronica.macedo@mottu.com.br>
2025-04-11 21:28:37 -0300

    escrevendo a palavra

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// EXERCICIO 3. Verificação de Palíndromo 
+// Enunciado: Escreva um programa que leia uma string e verifique se ela é um palíndromo
+//(palavra que se lê da mesma forma de trás para frente). O programa deve ignorar espaços e considerar
+//maiúsculas e minúsculas como iguais. 
+
+using System;
+
+class Program
+{
+static void Main()
+    {
+        while (true)
+        {
+            menu();
+        }
+    }
+
+static void menu()
+{
+    Console.WriteLine("Olá! Vamos validar se a palavra é um palíndromo");
+    Console.WriteLine("Escreva a plavra desejada: ");
+    string palavra = Console.ReadLine();
+}
+
+}
